2d0e398471f6e015e58b39ea893fed3c5c9210c5
Kim Westesen <kim-westesen@web.de>
2017-09-12 23:30:17 +0200

    Setup symfony project

---
+++ src/AppBundle/Twig/AppExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Twig;
+
+/**
+ * Class AppExtension
+ *
+ * @package AppBundle\Twig
+ */
+class AppExtension extends \Twig\Extension\AbstractExtension
+{
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter('movement', [$this, 'movementFilter'])
+        ];
+    }
+
+    /**
+     * @param float $number
+     *
+     * @return string
+     */
+    public function movementFilter(float $number)
+    {
+        $color = 'green';
+
+        if ($number < 0) {
+            $color = 'red';
+        } elseif ($number == 0) {
+            $color = 'black';
+        }
+
+        return '<span style="color: ' . $color. '">' . $number . '</span>';
+    }
+}
